755ec32ee3d26b61215ffb66d018665c8a2eaaf2
莇生和宏 <kazuanzo@l.local>
2018-09-02 16:14:32 +0900

    test

--- src/main/java/bulletinBoard/domain/Account.java
+++ src/main/java/bulletinBoard/domain/Account.java
@@ -1,18 +1,13 @@
 package bulletinBoard.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -25,7 +20,7 @@ public class Account implements Serializable{
 
 
 	/**
-	 * 
+	 * kkkkkkk
 	 */
 	private static final long serialVersionUID = 1L;
 	@Id
